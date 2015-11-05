e4bacf714b4fa48a131e814fb667551b06db4e1d
shimomura <shimomura@usa-mimi.jp>
2015-11-05 22:24:46 +0900

    Questionにinline追加

--- tutorial/polls/admin.py
+++ tutorial/polls/admin.py
@@ -4,5 +4,13 @@ from .models import Question
 from .models import Choice
 
 
-admin.site.register(Question)
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [ChoiceInline]
+
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
