e3d50fe49aad53c5934244e1a7a25d296360da29
shimomura <shimomura@usa-mimi.jp>
2015-11-05 23:03:54 +0900

    was_published_recentlyメソッド追加

--- tutorial/polls/admin.py
+++ tutorial/polls/admin.py
@@ -11,7 +11,7 @@ class ChoiceInline(admin.StackedInline):
 
 class QuestionAdmin(admin.ModelAdmin):
     inlines = [ChoiceInline]
-    list_display = ('pk', 'question_text', 'pub_date')
+    list_display = ('pk', 'question_text', 'pub_date', 'was_published_recently')
 
 admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
