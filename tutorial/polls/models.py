7475329fa5b9272fc018c64ac8a21ddcf96fa91d
shimomura <shimomura@usa-mimi.jp>
2015-11-05 17:54:56 +0900

    モデル名を変更してみる

--- tutorial/polls/models.py
+++ tutorial/polls/models.py
@@ -2,6 +2,10 @@ from django.db import models
 
 
 class Question(models.Model):
+    class Meta:
+        verbose_name = '質問'
+        verbose_name_plural = '質問の複数形'
+
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
