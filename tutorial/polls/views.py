1ade762b186137bb0f1ae16450762044c9d7505c
shimomura <shimomura@usa-mimi.jp>
2015-11-06 13:30:03 +0900

    templateへの値渡しテスト

--- tutorial/polls/views.py
+++ tutorial/polls/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'polls/index.html', {})
+    return render(request, 'polls/index.html', {
+        'hoge': 'test string',
+        'fuga': '<br>tag</br>',
+    })
