18eb3f685d824447c22af4d4a1484ca348e51d7b
shimomura <shimomura@usa-mimi.jp>
2015-11-06 12:48:42 +0900

    viewを書き換え，templateを使用してresponseを返すように修正

--- tutorial/polls/views.py
+++ tutorial/polls/views.py
@@ -1,5 +1,5 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return render(request, 'polls/index.html', {})
