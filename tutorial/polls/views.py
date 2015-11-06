c49d0c635a6f3f7ccf904ebce0e016267e1484df
shimomura <shimomura@usa-mimi.jp>
2015-11-06 13:40:48 +0900

    mark_safeのテスト

--- tutorial/polls/views.py
+++ tutorial/polls/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from django.utils.html import mark_safe
 
 
 def index(request):
     return render(request, 'polls/index.html', {
         'hoge': 'test string',
         'fuga': '<br>tag</br>',
+        'piyo': mark_safe('<br>tag</br>'),
     })
