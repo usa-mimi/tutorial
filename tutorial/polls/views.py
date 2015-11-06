c317af33d53bae43e10c2c78768ece13c08eb717
shimomura <shimomura@usa-mimi.jp>
2015-11-06 14:43:01 +0900

    詳細用viewとhtmlを追加

--- tutorial/polls/views.py
+++ tutorial/polls/views.py
@@ -7,3 +7,9 @@ def index(request):
     return render(request, 'polls/index.html', {
         'questions': Question.objects.all(),
     })
+
+
+def detail(request, pk):
+    return render(request, 'polls/detail.html', {
+        'question': Question.objects.get(pk=pk)
+    })
