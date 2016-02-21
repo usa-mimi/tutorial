34f49147acf9c45bef5f3ec4f868701f5a1ff0c8
shimomura <shimomura@usa-mimi.jp>
2016-02-21 17:04:29 +0900

    テスト用のFormをブラウザから見えるようにviews.py, urls.pyを設定，テンプレートを用意．
    http://localhost:8000/polls/form
    で確認できる

--- tutorial/polls/urls.py
+++ tutorial/polls/urls.py
@@ -5,6 +5,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^form$', views.form_test),
     url(r'(?P<pk>\d+)/$', views.detail, name='detail'),
     url(r'(?P<pk>\d+)/vote$', views.vote, name='vote'),
     url(r'(?P<pk>\d+)/results$', views.results, name='results'),
--- tutorial/polls/views.py
+++ tutorial/polls/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.shortcuts import get_object_or_404
 from django.shortcuts import redirect
 
+from .forms import MyForm
 from .models import Question
 from .models import Choice
 
@@ -9,6 +10,13 @@ from .models import Choice
 def index(request):
     return render(request, 'polls/index.html', {
         'questions': Question.objects.all(),
+    })
+
+
+def form_test(request):
+    form = MyForm()
+    return render(request, 'polls/form.html', {
+        'form': form,
     })
 
 
