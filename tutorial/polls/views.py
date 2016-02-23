1efe74c527f50b3613f77ff9e22aecfe0006fef2
shimomura <shimomura@usa-mimi.jp>
2016-02-23 16:35:21 +0900

    detailをクラスベースViewに置き換え．

--- tutorial/polls/views.py
+++ tutorial/polls/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
+from django.shortcuts import resolve_url
 from django.shortcuts import get_object_or_404
-from django.shortcuts import redirect
 from django.core.urlresolvers import reverse_lazy
 from django.views.generic import FormView
+from django.views.generic.detail import SingleObjectMixin
 
 from .forms import MyForm
 from .forms import VoteForm
@@ -20,22 +21,36 @@ class FormTest(FormView):
     template_name = 'polls/form.html'
     success_url = reverse_lazy('polls:index')
 
-
 form_test = FormTest.as_view()
 
-def detail(request, pk):
-    obj = get_object_or_404(Question, pk=pk)
-    if request.method == "POST":
-        form = VoteForm(question=obj, data=request.POST)
-        if form.is_valid():
-            form.vote()
-            return redirect('polls:results', pk)
-    else:
-        form = VoteForm(question=obj)
-    return render(request, 'polls/detail.html', {
-        'form': form,
-        'question': obj,
-    })
+
+class Detail(SingleObjectMixin, FormView):
+    model = Question
+    form_class = VoteForm
+    context_object_name = 'question'
+    template_name = 'polls/detail.html'
+
+    def get(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        return super().get(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        return super().post(request, *args, **kwargs)
+
+    def get_form_kwargs(self):
+        kwargs = super().get_form_kwargs()
+        kwargs['question'] = self.object
+        return kwargs
+
+    def form_valid(self, form):
+        form.vote()
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return resolve_url('polls:results', self.kwargs['pk'])
+
+detail = Detail.as_view()
 
 
 def results(request, pk):
