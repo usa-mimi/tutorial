7d19e3951963c31efe08b4314b9bfcbc01669223
shimomura <shimomura@usa-mimi.jp>
2016-02-23 15:41:12 +0900

    form_testをFormViewで書き換え．

--- tutorial/polls/views.py
+++ tutorial/polls/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404
 from django.shortcuts import redirect
+from django.core.urlresolvers import reverse_lazy
+from django.views.generic import FormView
 
 from .forms import MyForm
 from .forms import VoteForm
@@ -13,17 +15,13 @@ def index(request):
     })
 
 
-def form_test(request):
-    if request.method == "POST":
-        form = MyForm(data=request.POST)
-        if form.is_valid():
-            pass
-    else:
-        form = MyForm()
-    return render(request, 'polls/form.html', {
-        'form': form,
-    })
+class FormTest(FormView):
+    form_class = MyForm
+    template_name = 'polls/form.html'
+    success_url = reverse_lazy('polls:index')
 
+
+form_test = FormTest.as_view()
 
 def detail(request, pk):
     obj = get_object_or_404(Question, pk=pk)
