e722778ee21691713244c5064db03474cdc6198e
shimomura <shimomura@usa-mimi.jp>
2016-02-24 13:32:12 +0900

    poll用baseテンプレート用意，base.htmlのリンク，文言修正

--- tutorial/tutorial/urls.py
+++ tutorial/tutorial/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 
+from polls.views import index
+
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^polls/', include('polls.urls', namespace='polls')),
+    url(r'^$', index, name='index'),
 ]
