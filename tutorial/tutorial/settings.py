1f19af40e044c9eb53f10daeb1399a2d748bd852
shimomura <shimomura@usa-mimi.jp>
2016-02-24 14:48:55 +0900

    builtin設定を追加

--- tutorial/tutorial/settings.py
+++ tutorial/tutorial/settings.py
@@ -66,6 +66,9 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
+            'builtins': [
+                'bootstrap3.templatetags.bootstrap3',
+            ],
         },
     },
 ]
