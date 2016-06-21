4301169d6eb1a06e01d93282fbaab0f1fc2c367e
shimomura <shimomura@usa-mimi.jp>
2016-06-21 17:46:58 +0900

    値比較用テストの追加

--- tutorial/polls/tests.py
+++ tutorial/polls/tests.py
@@ -2,5 +2,9 @@ from django.test import TestCase
 
 
 class PollsTest(TestCase):
-    def test_hoge(self):
-        print('test!')
+    def test_success(self):
+        self.assertEqual(1, 1)
+        self.assertEqual(1, True)
+
+    def test_failed(self):
+        self.assertNotEqual(0, False)
